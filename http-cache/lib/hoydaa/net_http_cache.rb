27840830fcd31ec257889d370d3262631f47b9fc
Michal Mitter <mittermichal@gmail.com>
2014-06-07 10:30:15 +0200

    caching post

--- http-cache/lib/hoydaa/net_http_cache.rb
+++ http-cache/lib/hoydaa/net_http_cache.rb
@@ -5,19 +5,50 @@ module Net
   class HTTP
 
     def HTTP.get(uri_or_host, path = nil, port = nil)
-      #binding.pry
       if (Hoydaa::Cache.cacheable?(uri_or_host))
         rtn = Hoydaa::Cache.cached?(uri_or_host)
         if rtn 
           p "cached! :)"
           return rtn
         end
+        p "c downloading..."
         rtn = get_response(uri_or_host, path, port).body
         Hoydaa::Cache.store(uri_or_host, rtn)
         rtn
       else
         p "downloading..."
         get_response(uri_or_host, path, port).body
+      end
+    end
+
+    def HTTP.post_form(uri_or_host, params)
+      uri_post = uri_or_host#.to_s+params.sort.to_s
+      #{:test => 1, :aaa => 2}.sort.each do |k,v| '#{k}=#{v}&' end
+      if (Hoydaa::Cache.cacheable?(uri_post))
+        rtn = Hoydaa::Cache.cached?(uri_post)
+        if rtn 
+          p "cached! :)"
+          return rtn
+        end
+
+      p "c downloading..."
+      req = Post.new(uri_or_host)
+      req.form_data = params
+      req.basic_auth url.user, url.password if url.user
+      ret = new(url.hostname, url.port).start {|http|
+          http.request(req)
+      }.body
+
+        Hoydaa::Cache.store(uri_post, rtn)
+        rtn
+      else
+        p "downloading..."
+      req = Post.new(uri_or_host)
+      req.form_data = params
+      req.basic_auth url.user, url.password if url.user
+      new(url.hostname, url.port).start {|http|
+          http.request(req)
+      }.body
       end
     end
 
--- main.rb
+++ main.rb
@@ -11,23 +11,18 @@ require './http-cache/lib/hoydaa/file_store'
 def get(url)
 	Net::HTTP.get(URI(url))	
 end
+
+def post(url,params)
+	Net::HTTP.post_form(URI(url),params)	
+end
 # m = new Middleman
 # m.options[:verbose] = true
 
 
 #Hoydaa::Cache.clear_cache!
-Hoydaa::Cache.cache(/.*/, :expires => 10, :store => Hoydaa::FileStore.new("./cache/",5))
+Hoydaa::Cache.cache(/.*/, :expires => -1, :store => Hoydaa::FileStore.new("./cache/",5))
 
 get('http://www.gamestv.org/event/48176-outraged-esports-vs-sstat/statistics/')
 get('http://www.gamestv.org/event/48176-outraged-esports-vs-sstat/statistics/')
-get('http://www.gamestv.org/event/48176-outraged-esports-vs-sstat/statistics/')
-get('http://www.gamestv.org/event/48176-outraged-esports-vs-sstat/statistics/')
-get('http://www.gamestv.org/event/48176-outraged-esports-vs-sstat/statistics/')
-get('http://www.gamestv.org/event/48176-outraged-esports-vs-sstat/statistics/')
-get('http://www.gamestv.org/event/48176-outraged-esports-vs-sstat/statistics/')
-get('http://www.gamestv.org/event/48176-outraged-esports-vs-sstat/statistics/')
-get('http://www.gamestv.org/event/48176-outraged-esports-vs-sstat/statistics/')
-get('http://www.gamestv.org/event/48176-outraged-esports-vs-sstat/statistics/')
-get('http://www.gamestv.org/event/48176-outraged-esports-vs-sstat/statistics/')
-get('http://www.gamestv.org/event/48176-outraged-esports-vs-sstat/statistics/')
-get('http://www.gamestv.org/event/48176-outraged-esports-vs-sstat/statistics/')
+post('http://www.gamestv.org/event/48176-outraged-esports-vs-sstat/statistics/',:asd => 1, :dsa => 2)
+post('http://www.gamestv.org/event/48176-outraged-esports-vs-sstat/statistics/',:dsa => 2 ,:asd => 1)
